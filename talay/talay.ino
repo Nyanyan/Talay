afee7d6d544d00099dc3f9ea6a53c901877d0ec8
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-11-30 20:04:00 +0900

    modified program

--- talay/talay.ino
+++ talay/talay.ino
@@ -1,18 +1,30 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(A0, A1); // RX, TX
+SoftwareSerial serial0(A0, A1); // RX, TX
+//SoftwareSerial serial1(A2, A3); // RX, TX
+//SoftwareSerial serial2(A4, A5); // RX, TX
+
+bool flag = true;
+int num = 0;
 
 void setup() {
   Serial.begin(115200);
-  mySerial.begin(1200);
+  serial0.begin(1200);
 
 }
 
 void loop() {
-  mySerial.listen();
-  String input = mySerial.readStringUntil(char(10));
-  bool flag = false;
-  if (input[0] == 'S') {
+  serial0.listen();
+  String input = serial0.readStringUntil(char(10));
+  if (input[0] == 'S' and flag) {
+    int checksum = 64;
+    for (int i = 0; i < 6; i++)
+      checksum += int(input[i + 1]) - 48;
+    if (char(checksum) == input[7]) {
+      flag = false;
+      Serial.print(char(48));
+      Serial.print(',');
+      Serial.println(input);
+    }
+  } else if (input[0] == 'I')
     flag = true;
-    Serial.println(input);
-  }
 }
